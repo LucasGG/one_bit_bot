f9df789f793688e2567e52fd06d3219780eb3f1d
Lucas Guimarães Gonçalves <lucasggcel@gmail.com>
2020-02-22 13:59:57 -0300

    Adjust rubocop and apply autocorrect

--- config/database.rb
+++ config/database.rb
@@ -26,11 +26,11 @@ configure :production do
   )
 
   set :database, {
-    adapter: 'postgresql',
-    host: db.host,
-    username: db.user,
-    password: db.password,
-    database: db.path[1..-1],
-    encoding: 'utf8'
+    :adapter => 'postgresql',
+    :host => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
   }
 end
